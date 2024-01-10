2a7298af910f20d77e139cbeecb39e29a643b454
LucasMarques02 <137355508+LucasMarques02@users.noreply.github.com>
2024-01-10 16:42:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -38,6 +38,30 @@ function color(){
     }
 
 }
+      
+
+function theme1(){
+        divisao.style.backgroundColor = "#323741";
+        multiplicacao.style.backgroundColor = "#323741";
+        adicao.style.backgroundColor = "#323741";
+        subtracao.style.backgroundColor = "#323741";
+
+        divisao.style.borderColor = "red";
+        multiplicacao.style.borderColor = "red";
+        subtracao.style.borderColor = "red";
+        adicao.style.borderColor = "red";
+
+        input1.style.borderColor = "red";
+        input2.style.borderColor = "red";
+
+        all.style.backgroundImage = "none";
+        all.style.backgroundColor = "rgba(59, 65, 77, 1)";
+
+        all.style.borderColor =  "red";
+        span1.style.color = "linear-gradient(to top left, rgb(48, 31, 194), rgb(52, 74, 202), rgb(0, 225, 255));";
+        redColorStatus = false; 
+
+}
 
 function redColor(){
     all.style.borderColor = "#500b0b";
@@ -112,3 +136,43 @@ divisao.addEventListener("click", divide);
 multiplicacao.addEventListener("click", multiplica);
 adicao.addEventListener("click", adiciona);
 subtracao.addEventListener("click", subtrai);
+
+document.addEventListener("keydown", function(event){
+    if(event.key=="L"||event.key=="l"){
+        trocadorDeTemas();
+    }
+});
+
+var contador = new Array(10);
+
+contador[0] = theme1;
+contador[1] = color;
+contador[2] = redColor;
+
+
+const numeroDeTemas = 2;
+let contador1 = 0;
+
+
+function trocadorDeTemas(){
+    if(contador1==0){
+        contador[0]();
+    }
+    if(contador1==1){
+        contador[1]();
+    }
+    if(contador1==2){
+        contador[2]();
+    }
+    contador++;
+    if(contador1==numeroDeTemas){
+        contador = 0;
+    }
+    
+}
+
+
+
+
+
+"function Obstacle(\n    canvasCtx, type, spriteImgPos, dimensions, gapCoefficient, speed,\n    opt_xOffset, opt_isAltGameMode) {\n  this.canvasCtx = canvasCtx;\n  this.spritePos = spriteImgPos;\n  this.typeConfig = type;\n  this.gapCoefficient = Runner.slowDown ? gapCoefficient * 2 : gapCoefficient;\n  this.dimensions = dimensions;\n  this.remove = false;\n  this.xPos = dimensions.WIDTH + (opt_xOffset || 0);\n  this.yPos = 0;\n  this.width = 0;\n  this.gap = 0;\n  this.speedOffset = 0;\n  this.altGameModeActive = opt_isAltGameMode;\n  this.imageSprite = this.typeConfig.type == 'COLLECTABLE' ?\n      Runner.altCommonImageSprite :\n      this.altGameModeActive ? Runner.altGameImageSprite : Runner.imageSprite;\n\n  // For animated obstacles.\n  this.currentFrame = 0;\n  this.timer = 0;\n\n  this.init(speed);\n}"
